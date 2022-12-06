49d3f8e11c074acd30f031cafd96c568d5745176
leekayden <kayden@programmer.net>
2022-12-06 19:47:30 +0800

    Initial commit

---
+++ .evershop/build/frontStore/categoryView/client/entry.js
@@ -0,0 +1,82 @@
+
+      import React from 'react';
+      import ReactDOM from 'react-dom';
+      import Area from '@evershop/evershop/src/lib/components/Area';
+      import Hydrate from '@evershop/evershop/src/lib/components/react/client/Hydrate';
+      
+Area.defaultProps.components = {
+  content: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xGaWx0ZXIuanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xGaWx0ZXIuanM=',
+      sortOrder: 1,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\catalog\\pages\\frontStore\\categoryView\\Filter.js')
+    },
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xHZW5lcmFsLmpz: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xHZW5lcmFsLmpz',
+      sortOrder: 15,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\catalog\\pages\\frontStore\\categoryView\\General.js')
+    },
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xQYWdpbmF0aW9uLmpz: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xQYWdpbmF0aW9uLmpz',
+      sortOrder: 20,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\catalog\\pages\\frontStore\\categoryView\\Pagination.js')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xQcm9kdWN0cy5qcw==': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2F0YWxvZ1xwYWdlc1xmcm9udFN0b3JlXGNhdGVnb3J5Vmlld1xQcm9kdWN0cy5qcw==',
+      sortOrder: 15,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\catalog\\pages\\frontStore\\categoryView\\Products.js')
+    }
+  },
+  'icon-wrapper': {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2hlY2tvdXRccGFnZXNcZnJvbnRTdG9yZVxhbGxcTWluaUNhcnQuanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2hlY2tvdXRccGFnZXNcZnJvbnRTdG9yZVxhbGxcTWluaUNhcnQuanM=',
+      sortOrder: 10,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\checkout\\pages\\frontStore\\all\\MiniCart.js')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY3VzdG9tZXJccGFnZXNcZnJvbnRTdG9yZVxhbGxcVXNlckljb24uanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY3VzdG9tZXJccGFnZXNcZnJvbnRTdG9yZVxhbGxcVXNlckljb24uanM=',
+      sortOrder: 100,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\customer\\pages\\frontStore\\all\\UserIcon.js')
+    }
+  },
+  body: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExheW91dC5qcw==': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExheW91dC5qcw==',
+      sortOrder: 1,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Layout.js')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE5vdGlmaWNhdGlvbi5qcw==': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE5vdGlmaWNhdGlvbi5qcw==',
+      sortOrder: 10,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Notification.js')
+    }
+  },
+  header: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExvZ28uanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExvZ28uanM=',
+      sortOrder: 1,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Logo.js')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1lbnUuanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1lbnUuanM=',
+      sortOrder: 5,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Menu.js')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1vYmlsZU1lbnUuanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1vYmlsZU1lbnUuanM=',
+      sortOrder: 5,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\MobileMenu.js')
+    }
+  },
+  head: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1ldGEuanM=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1ldGEuanM=',
+      sortOrder: 5,
+      component: require('C:\\Users\\kayde\\Desktop\\000_projects\\shopfront\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Meta.js')
+    }
+  }
+} 
+ReactDOM.hydrate(
+        <Hydrate/>,
+        document.getElementById('app')
+      );
